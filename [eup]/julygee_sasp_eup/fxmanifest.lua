e941ff176f85e5d85dc88233aa543b49917ef1c3
FAFO Box <jmfulton23@gmail.com>
2024-09-02 18:18:10 -0500

    ALLOT Part 4

---
+++ [eup]/julygee_sasp_eup/fxmanifest.lua
@@ -0,0 +1,19 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+name 'JulyGee SASP EUP Megapack'
+version '2.0.0'
+author 'JulyGee'
+description 'A male and female friendly EUP Megapack based on San Andreas State Police'
+
+files {
+  'mp_m_freemode_01_mp_m_julygee_sasp_eup.meta',
+  'mp_f_freemode_01_mp_f_julygee_sasp_eup.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_julygee_sasp_eup.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_julygee_sasp_eup.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
