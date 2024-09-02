b9fec38b6b434c5ef600f140250cf34a1efc99c4
FAFO Box <jmfulton23@gmail.com>
2024-09-02 18:20:28 -0500

    ALLOT Part 5

---
+++ [eup]/julygee_lcso_eup/fxmanifest.lua
@@ -0,0 +1,19 @@
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+-- Resource Information --
+name 'JulyGee LCSO EUP Megapack'
+version '1.0.0'
+author 'JulyGee'
+description 'A male and female friendly EUP Megapack based on Los Santos County Sheriffs Office'
+
+files {
+  'mp_m_freemode_01_mp_m_julygee_lcso_eup.meta',
+  'mp_f_freemode_01_mp_f_julygee_lcso_eup.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_julygee_lcso_eup.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_julygee_lcso_eup.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
