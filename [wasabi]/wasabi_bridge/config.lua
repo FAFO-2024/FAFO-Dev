ab71cdc727d61f2dd20c9c6fdc1377ca9632f120
FAFO-2024 <167828552+FAFO-2024@users.noreply.github.com>
2024-09-02 18:10:03 -0500

    ALLOT Part 1
    Its been a while

---
+++ [wasabi]/wasabi_bridge/config.lua
@@ -0,0 +1,30 @@
+-----------------For support, scripts, and more----------------
+--------------- https://discord.gg/wasabiscripts  -------------
+---------------------------------------------------------------
+Config = {}
+
+-------------------------------------------------------
+-- Check for updates? (RECOMMENDED TO LEAVE AS true) --
+-------------------------------------------------------
+Config.CheckForUpdates = true
+
+-------------------------------------------------------------------
+-- Notification sound (Only works with wasabi_notify by default) --
+-- Grab it here: https://store.wasabiscripts.com/package/6215100 --
+-------------------------------------------------------------------
+Config.NotificationSound = true -- Play notification sound? If using wasabi_notify
+
+------------------------------------------------------------------
+-- Notification location on screen(If using default ox notify)  --
+-- Options: 'top', 'top-right', 'top-left', 'bottom',           --
+-- 'bottom-right', 'bottom-left', 'center-right', 'center-left' --
+------------------------------------------------------------------
+Config.NotificationLocation = 'top-right'
+
+---------------------------------------------------------
+--     Specify the target system being used here.      --    
+-- Options are 'qb-target', 'qtarget', and 'ox_target' --
+-- Set to false if not using any of the above systems. --
+--           OR add your own target system!            --
+---------------------------------------------------------
+Config.TargetSystem = 'qb-target'
